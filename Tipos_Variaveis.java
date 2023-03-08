daea1ec126d7c19cbb034a982810d726fdaae1a9
SamirGuimaraes <126697620+SamirGuimaraes@users.noreply.github.com>
2023-03-08 20:09:22 -0300

    Add files via upload

--- Tipos_Variaveis.java
+++ Tipos_Variaveis.java
@@ -1,3 +1,52 @@
 public class Tipos_Variaveis {
     
+/*Atribuição
+O operador de atribuição, é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável onde em Java definimos um tipo, nome e opcionalmente atribuímos um valor à variável.
+//classe Operadores.java
+String nome = "GLEYSON";
+int idade = 22;
+double peso = 68.5;
+char sexo = 'M';
+boolean doadorOrgao = false;
+Date dataNascimento = new Date(); */
+
+/*Tipos de dados
+No Java, existem algumas palavras reservadas, para a representação dos tipos de dados básicos, que precisam ser manipulados para a construção de programas. Estes tipos de dados são conhecidos como tipos primitivos.
+
+
+Números que podem conter partes fracionárias, podem ser representados por dois tipos:
+
+Observe que, o tipo long precisa terminar com L, o tipo float precisa terminar com F e alguns cenários do dia-a-dia podem estimular uma alteração de tipos de dados convencional.
+*/
+
+/*Variáveis e Constantes
+Na programação, uma variável é uma palavra (geralmente abreviada, mas não recomendado) que representa um valor arbitrário, não totalmente especificado, ou desconhecido.
+No Java, utilizamos identificadores que representam uma referência (ponteiro) a um valor em memória, e esta referência pode ser redirecionada a outro valor e assim por diante, por isso,  chamamos este identificador de variável, pois pode alterar o seu valor.
+Abaixo, temos um exemplo explicativo sobre uso de variáveis e constantes:
+public class ExemploVariavel {
+	public static void main(String[] args) {
+		/*
+		 * esta linha é considerada como declaração de variável iniciamos a existência
+		 * variavel numero com valor 5 regra: tipo + nome + valor
+		 */
+		//int numero = 5;
+
+		/*
+		 * na linha abaixo iremos alterar o valor do varíavel para 10 observe que o tipo
+		 * não é mais necessário, pois a variável já foi declarada anteriormente
+		 */
+		//numero = 10;
+
+		//System.out.print(numero);
+		
+		/*
+		 * ao usar a palavra reservada final, você determina que jamais
+		 * esta variavel poderá obter outro valor;
+		 * logo a linha 25 vai apresentar um erro de compilação
+		 * isso é considerado uma CONSTANTE na linguagem Java
+		 */
+		//final double pi = 3.14;
+		
+		//pi=3.15; */
+
 }
